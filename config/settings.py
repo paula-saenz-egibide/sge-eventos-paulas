025687a46d3fdb24162afbafd5ad1098abbc69a4
paulalopezruiz <paula.lopez51397@ikasle.egibide.org>
2026-05-16 14:26:43 +0200

    Reactivar inscripción cancelada

--- config/settings.py
+++ config/settings.py
@@ -72,8 +72,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(weeks=3),
+    'REFRESH_TOKEN_LIFETIME': timedelta(weeks=4),
 }
 
 
